613ad3ee2f1c0275624e8333de39e64f490fa92c
Andrzej Węgrzyn <andrzej9393@gmail.com>
2016-06-27 00:21:28 +0200

    buttons disabled/enabled functionality

--- logic.js
+++ logic.js
@@ -1,4 +1,34 @@
 angular.module('Main',['ngMaterial']).controller('Ctrl',['$scope','$interval',function($scope,$interval){
+	$scope.Panel = {
+			StartDisabled: false,
+			StopDisabled: true,
+			UpPercentDisabled: false,
+			IntervalDisabled: false,
+			InitLatticeDisabled: false,
+			onlyStopEnabled: function(){
+				this.StartDisabled =  true;
+				this.StopDisabled = false;
+				this.UpPercentDisabled = true;
+				this.IntervalDisabled = true;
+				this.InitLatticeDisabled = true;
+			},
+			onlyStopDisabled: function(){
+				this.StartDisabled =  false;
+				this.StopDisabled = true;
+				this.UpPercentDisabled = false;
+				this.IntervalDisabled = false;
+				this.InitLatticeDisabled = false;
+			}
+	}
+	this.event = {
+			start: function(){
+				$scope.Panel.onlyStopEnabled(); 
+			},
+			stop: function(){
+				$scope.Panel.onlyStopDisabled(); 
+			}
+			
+	}
 				
 			
 	}]);
